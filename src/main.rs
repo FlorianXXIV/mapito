6ecc362d6dba575324f0af15bfc76e21785f2e5a
FlorianXXIV <info.github@florianxxiv.dev>
2024-11-02 19:52:30 +0100

    Download with ID is possible

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::path::Path;
 
 use argparse::{parser, ArgumentParser, Store, StoreConst, StoreTrue};
 use reqwest::{blocking::Client, Url};
@@ -23,6 +24,7 @@ fn main() {
     let mut staging = 1;
     let mut search: String = String::new();
     let mut dl_path: String = env::var("HOME").unwrap() + "/Downloads";
+    let mut dl_id: String = String::new();
     //argument parser arg/opt setup
     {
         let mut parser = ArgumentParser::new();
@@ -38,6 +40,8 @@ fn main() {
         );
         parser.refer(&mut dl_path)
             .add_option(&["-p","--path"], Store, "The path where you want to download any files to. Default: ~/Downloads");
+        parser.refer(&mut dl_id)
+            .add_option(&["-d","--download"], Store, "Download the mod given by ID.");
 
         parser.parse_args_or_exit();
     }
@@ -60,5 +64,17 @@ fn main() {
         for hit in query_response.hits {
             println!("----\nName: {}, Latest Version: {}\n\n{}\n\nAuthor: {}\nId: {}\nDownloads: {}\n\n",hit["title"],hit["versions"][0],hit["description"], hit["author"], hit["project_id"], hit["downloads"]);
         }
+    }
+
+    if !dl_id.is_empty() {
+        let query = Url::parse(&(API_URL[staging].to_owned() + "v2/project/" + &dl_id)).unwrap();
+        let query_response: Value = serde_json::from_str(&client.get(query).send().unwrap().text().unwrap()).unwrap();
+        let latest_version = query_response["versions"][0].clone();
+        let query = Url::parse(&(API_URL[staging].to_owned() + "v2/version/" + latest_version.as_str().unwrap())).unwrap();
+        let download_url: Value = serde_json::from_str(&client.get(query).send().unwrap().text().unwrap()).unwrap();
+        let body = client.get(download_url["files"][0]["url"].as_str().unwrap()).send().unwrap().bytes().unwrap();
+        let filename = download_url["files"][0]["filename"].as_str().unwrap();
+        let path = &(dl_path + "/" + filename);
+        let _ = std::fs::write(path, &body);
     }
 }
