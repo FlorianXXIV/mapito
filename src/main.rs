8554b922f47280d88ac0703ea4a92360b5d62d3c
FlorianXXIV <mail@florianxxiv.dev>
2025-01-27 16:10:07 +0100

    Add option to download project for specified mc version

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod client;
 
 use crate::client::Downloader;
+use core::panic;
 use std::env;
 use colored::Colorize;
 
@@ -34,6 +35,7 @@ fn main() {
     let mut search: String = String::new();
     let mut dl_path: String = env::var("HOME").unwrap() + "/Downloads";
     let mut dl_id: String = String::new();
+    let mut mc_ver: String = String::new();
     let mut pack_path: String = String::new();
     //argument parser arg/opt setup
     {
@@ -63,7 +65,11 @@ fn main() {
             Store,
             "Download the mod given by it's ID/Slug.",
         );
-
+        parser.refer(&mut mc_ver).add_option(
+            &["-v", "--mc-ver"], 
+            Store,
+            "Set the Minecraft version that you want to download the Mod for."
+        );
         parser.parse_args_or_exit();
     }
 
@@ -74,19 +80,14 @@ fn main() {
     }
 
     if !dl_id.is_empty() {
-        let query = Url::parse(&(API_URL[staging].to_owned() + PROJECT + "/" + &dl_id)).unwrap();
-        let query_response: Value =
-            serde_json::from_str(&client.get(query).send().unwrap().text().unwrap()).unwrap();
-        let latest_version = query_response["versions"][0].clone();
-        let query = Url::parse(
-            &(API_URL[staging].to_owned() + VERSION + "/" + latest_version.as_str().unwrap()),
-        )
-        .unwrap();
-        let download_url: Value =
-            serde_json::from_str(&client.get(query).send().unwrap().text().unwrap()).unwrap();
+        let download_url: Value = get_dl_url(dl_id, &client, mc_ver, staging).expect("get_dl_url");
         let filename = download_url["files"][0]["filename"].as_str().unwrap();
         let path = &(dl_path + "/" + filename);
-        let _ = client.download_file(path, download_url["files"][0]["url"].as_str().unwrap()).unwrap();
+        let _ = client
+            .download_file(
+                path,
+                download_url["files"][0]["url"].as_str().unwrap()
+            ).unwrap();
     }
 }
 
@@ -117,4 +118,51 @@ fn search_package(client: &Client, query: String, staging: usize) {
                 hit["description"].to_string().replace("\"", "").bright_black(),
             );
         }
+}
+
+fn get_dl_url(dl_id: String, client: &Client, mc_ver: String, staging: usize) 
+    -> Result<Value, &str> 
+{
+    let query = Url::parse(
+        &(API_URL[staging].to_owned() + PROJECT + "/" + &dl_id)
+        ).unwrap();
+
+    let project: Value =
+        serde_json::from_str(
+            &client.get(query).send().unwrap().text().unwrap()
+        ).unwrap();
+    
+    let versions = project["versions"].as_array().unwrap();
+    let mut download_url: Value = Value::Null;
+    if mc_ver.is_empty() {
+        let latest_version = versions.last().unwrap().clone();
+        let query = Url::parse(
+            &(API_URL[staging].to_owned() + VERSION + "/" + latest_version.as_str().unwrap()),
+        ).unwrap();
+
+        download_url =
+            serde_json::from_str(&client.get(query).send().unwrap().text().unwrap()).unwrap();
+    } else {
+        if project["game_versions"].as_array().unwrap().iter().any(|e| *e == *mc_ver) {
+            for version in versions.into_iter().rev() {
+                let query = Url::parse(
+                    &(API_URL[staging].to_owned() + VERSION + "/" + version.as_str().unwrap())
+                ).unwrap();
+                let resp_ver: Value = 
+                    serde_json::from_str(
+                        &client.get(query).send().unwrap().text().unwrap()
+                    ).unwrap();
+                if resp_ver["game_versions"].as_array().unwrap().iter().any(|e| *e == *mc_ver) {
+                    download_url = resp_ver;
+                    break;
+                }
+            }
+        } else {
+            return Err("Minecraf version not Available!");
+        }
+    }
+    if download_url.is_null() {
+        return Err("Did not find Project");
+    }
+    return Ok(download_url);
 }
